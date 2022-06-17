3bd0d739020e62fc2e2291b7975fbb0e01ea4567
mohamedelleuch777 <usher7med@gmail.com>
2022-06-18 01:58:22 +0300

    login page html structure+basic css

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -4,5 +4,6 @@ import React from "react";
 export default function Dashboard(props) {
     return <div>
         fewfwe
+        <button>click me</button>
     </div>
 }
--- src/Login/index.js
+++ src/Login/index.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styles from './styles.module.css';
 
 
 export default function Login(props) {
-    return <div>
-        login
+
+    useEffect(()=>{
+        document.getElementsByTagName('body')[0].classList.add('no-extra');
+    },[]);
+
+    return (
+    <div>
+        <img src="/assets/img/bg.jpg" alt="background"/>
+        <div className={styles.externalSection}>
+            <h3 className={styles.title}>Login</h3>
+            <hr />
+            <div className={styles.inputsContainer}>
+                <input type="email" placeholder="Enter your email here:" />
+                <input type="password"  placeholder="Enter your password here:"/>
+                <div className={styles.flexSpaceBetween}>
+                    <div>
+                        <input type="checkbox" id="checkbox-remember" /><label fro="checkbox-remember">Remember me</label>
+                    </div>
+                    <a href="#">Forget Password</a>
+                </div>
+            </div>
+            <button>Login</button>
+            <hr />
+            <div className={styles.flexSpaceBetween}>
+                <p>
+                    Thank you and enjoy our website.<br/>
+                    <strong>Your Authentication Team</strong>
+                </p>
+                <img className={styles.smallLogo} src="https://technext.github.io/guruable/assets/images/auth/Logo-small-bottom.png" />
+            </div>
+        </div>
     </div>
+    )
 }
