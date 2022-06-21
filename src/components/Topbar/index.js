19a1dcc745ad92f9322319b42e1e400811a1366b
Elleuch <elleuch@media-saturn.com>
2022-06-21 13:33:12 +0300

    top bar design is ready

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -7,11 +7,12 @@ export default function Topbar (props) {
     return (
         <nav className={styles.navBar} >
             <section className={styles.blackSection}>
-                black section
+                <img alt="logo" className={styles.smallLogo} src="/assets/img/logo.png" />
+                <span className={styles.round}><i class="fa fa-bars" aria-hidden="true"></i></span>
             </section>
             <section className={styles.whiteSection}>
                 <div className={styles.leftItem}>
-
+                    <i class="fa fa-arrows-alt" aria-hidden="true"></i>
                 </div>
                 <div className={styles.rightItem}>
                     <i className="fa fa-bell-o" aria-hidden="true"></i>
