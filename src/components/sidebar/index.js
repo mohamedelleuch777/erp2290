9c6f3317bbe36b6965406091fbc988cf190e91db
Elleuch <elleuch@media-saturn.com>
2022-06-21 15:17:56 +0300

    profile badge added to sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,11 +4,14 @@ import styles from './styles.module.css';
 export default function Sidebar(props) {
     return (
     <div className={styles.sideBarContainer}>
-        <section>
+        <section className={styles.profileContainer}>
             <img className={styles.profilePicture} alt="profile-avatar" src="/assets/img/avatar.jpg" />
             <div>
                 <h3 className={styles.profileName}>Mohamed Elleuch</h3>
-                <i className="fa fa-angle-down" aria-hidden="true"></i>
+                <div className={styles.textContainer} >
+                    <span className={styles.profileAccountType} >Administrator</span>
+                    <i className="fa fa-angle-down" aria-hidden="true"></i>
+                </div>
             </div>
         </section>
     </div>
